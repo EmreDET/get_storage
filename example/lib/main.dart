ca2d5a7bdb76091e7aa688595291dc2ac4304339
Emre Yurtseven <emre.yurtseven@deinerstertag.de>
2025-06-03 10:09:09 +0200

    refactor: improve theme management and controller initialization

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:get_storage/get_storage.dart';
 
 void main() async {
   await GetStorage.init();
+  Get.put(Controller());
   runApp(App());
 }
 
@@ -12,16 +13,27 @@ class Controller extends GetxController {
   final box = GetStorage();
   bool get isDark => box.read('darkmode') ?? false;
   ThemeData get theme => isDark ? ThemeData.dark() : ThemeData.light();
-  void changeTheme(bool val) => box.write('darkmode', val);
+  void changeTheme(bool val) {
+    box.write('darkmode', val);
+    refresh();
+  }
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final Controller controller = Get.find<Controller>();
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(Controller());
-    return Observer(builder: (_) {
+    return GetBuilder<Controller>(builder: (controller) {
+      print("Rebuilding App with theme: ${controller.theme}");
+      print("Is dark mode: ${controller.isDark}");
       return MaterialApp(
         theme: controller.theme,
+        themeMode: controller.isDark ? ThemeMode.dark : ThemeMode.light,
         home: Scaffold(
           appBar: AppBar(title: Text("Get Storage")),
           body: Center(
